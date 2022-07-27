badac608ed28bafcf92198047e3927d51b081361
Daviiduhh <daviiduhhs@gmail.com>
2022-07-27 00:21:21 -0500

    Items colocados en tarjeta User

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,32 @@ function App() {
 
   return (
     <div className="App">
+      <User />
       <Lista />
     </div>
+  );
+}
+
+function User() {
+  const user = {
+    name: "David Madrid",
+    img: '../src/assets/user.JPG',
+    filters: ["Daily", "Weekly", "Monthly"],
+  };
+
+  return (
+    <>
+      <div className="user">
+        <div className="user__content">
+          <img className="user__img" src={user.img} alt="User image" />
+          <span className="user__subtitle">Report for</span>
+          <span className="user__name">{user.name}</span>
+        </div>
+        {user.filters.map((filter) => (
+          <span className="user__filter">{filter}</span>
+        ))}
+      </div>
+    </>
   );
 }
 
@@ -18,57 +42,61 @@ function Lista() {
       title: "work",
       hours: 32,
       pastHours: 36,
-      color: '#fe8b63'
+      color: "#fe8b63",
     },
     {
       id: 2,
       title: "play",
       hours: 10,
       pastHours: 8,
-      color: '#56c2e6'
+      color: "#56c2e6",
     },
     {
       id: 3,
       title: "study",
       hours: 4,
       pastHours: 7,
-      color: '#ff5e7d'
+      color: "#ff5e7d",
     },
     {
       id: 4,
       title: "excercise",
       hours: 4,
       pastHours: 5,
-      color: '#4ace82'
+      color: "#4ace82",
     },
     {
       id: 5,
       title: "social",
       hours: 5,
       pastHours: 10,
-      color: '#7235d1'
+      color: "#7235d1",
     },
     {
       id: 6,
       title: "self care",
       hours: 2,
       pastHours: 2,
-      color: '#f1c55a'
+      color: "#f1c55a",
     },
   ];
 
   const listActivities = elements.map((element) => (
     <>
-      <div className="activity" key={element.id} style={
-          {
-            backgroundColor: element.color
-          }
-        }>
+      <div
+        className="activity"
+        key={element.id}
+        style={{
+          backgroundColor: element.color,
+        }}
+      >
         <div className="activity__content">
           <span className="activity__title">{element.title}</span>
           <span>...</span>
           <span className="activity__hours">{element.hours}hrs</span>
-          <span className="activity__pasthours">last week - {element.pastHours}</span>
+          <span className="activity__pasthours">
+            last week - {element.pastHours}
+          </span>
         </div>
       </div>
     </>
@@ -76,7 +104,6 @@ function Lista() {
 
   return (
     <>
-      <h1>Tracked activities</h1>
       <section className="activities">{listActivities}</section>
     </>
   );
